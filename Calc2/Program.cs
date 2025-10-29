f152bd9a6aaaf08258444f39905b40c6082c3c7c
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2025-10-30 01:39:58 +0800

    Operations priority DONE

--- Calc2/Program.cs
+++ Calc2/Program.cs
@@ -16,7 +16,7 @@ namespace Calc2
 		static void Main(string[] args)
 		{
 			Console.Write("Введите арифметическое выражение: ");
-			string expression = "22*33/44/2*8*3";
+			string expression = "22+33-44/2+8*3";
 			//string expression = Console.ReadLine();
 			expression = expression.Replace(".", ",");
 			expression = expression.Replace(" ", "");
@@ -48,8 +48,8 @@ namespace Calc2
 
 			while (operations[0] != "")
 			{
-				int i = 0;
-				for (; i < operations.Length; i++)
+				//nt i = 0;
+				for (int i=0; i < operations.Length; i++)
 				{
 					if (operations[i] == "*" || operations[i] == "/")
 					{
@@ -61,21 +61,51 @@ namespace Calc2
 						{
 							values[i] /= values[i + 1];
 						}
+						for (int index = i; index < operations.Length - 1; ++index)
+						{
+							operations[index] = operations[index + 1];
+						}
+						for (int index = i + 1; index < values.Length - 1; ++index)
+						{
+							values[index] = values[index + 1];
+						}
+						operations[operations.Length - 1] = "";
+						values[values.Length - 1] = 0;
 					}
-					for (int index = i; index < operations.Length - 1; ++index)
+					
+					if (operations[i] == "*" || operations[i] == "/") i--;
+				}
+				for (int i=0; i < operations.Length; i++)
+				{
+					if (operations[i] == "+" || operations[i] == "-")
 					{
-						operations[index] = operations[index + 1];
+						if (operations[i] == "+")
+						{
+							values[i] += values[i + 1];
+						}
+						if (operations[i] == "-")
+						{
+							values[i] -= values[i + 1];
+						}
+						for (int index = i; index < operations.Length - 1; ++index)
+						{
+							operations[index] = operations[index + 1];
+						}
+						for (int index = i + 1; index < values.Length - 1; ++index)
+						{
+							values[index] = values[index + 1];
+						}
+						operations[operations.Length - 1] = "";
+						values[values.Length - 1] = 0;
 					}
-					for (int index = i + 1; index < values.Length - 1; ++index)
-					{
-						values[index] = values[index + 1];
-					}
-					operations[operations.Length - 1] = "";
-					values[values.Length - 1] = 0;
-					if(operations[i]=="*"|| operations[i]=="/")i--;
+					
+					if (operations[i] == "+" || operations[i] == "-") i--;
 				}
 			}
+			
 			Console.WriteLine(values[0]);
+		
+
 
 #if HOME_CHECK
 		while (operations.Contains("*") || operations.Contains("/"))
